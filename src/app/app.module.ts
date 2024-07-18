69200a6de78c94966038765df3f8b806fba82ce5
Juan Felipe Herrera Pinzón <77707590+FelipeHerreraPinzon@users.noreply.github.com>
2024-07-17 23:46:11 -0500

    rutas listas !!!

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { ProductosComponent } from './productos/productos.component';
+import { VentasComponent } from './ventas/ventas.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    ProductosComponent,
+    VentasComponent
   ],
   imports: [
     BrowserModule,
